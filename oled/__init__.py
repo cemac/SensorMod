8da93ff6aa2528cbb52b7c09a890bda648a8e26f
Daniel Ellis <daniel.ellis.research@gmail.com>
2020-12-07 23:30:46 +0000

    AutoCommit sleep bypass test

--- oled/__init__.py
+++ oled/__init__.py
@@ -35,9 +35,24 @@ def updatedata(date, dct):
 
         draw.text((x, top + 0), "%20s"%date, font=font, fill=255)
         
-        draw.text((x, top + 8), 'PM1   % .2e| TEMP % 4dC'%(dct[4],dct[7]), font=font, fill=255)
-        draw.text((x, top + 16),'PM2.5 %.2e| RH % 6d'%(dct[5],dct[8]), font=font, fill=255)
-        draw.text((x, top + 25), 'PM10  % .2e| X % 7d'%(dct[6],dct[10]), font=font, fill=255)
+        draw.text((x, top + 8), '  | PM1   % .2e | '%(dct[4]), font=font, fill=255)
+        draw.text((x, top + 16),'  | PM2.5  %.2e | '%(dct[5]), font=font, fill=255)
+        draw.text((x, top + 25), '  | PM10  % .2e | '%(dct[6]), font=font, fill=255)
+        
+        # Display image.
+        disp.image(image)
+        disp.show()
+        
+
+def otherdata(date, dct):
+        #clear
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+        draw.text((x, top + 0), "%20s"%date, font=font, fill=255)
+        
+        draw.text((x, top + 8), '  | TEMP % 4dC | '%(dct[7]), font=font, fill=255)
+        draw.text((x, top + 16),'  | RH % 6d | '%(dct[8]), font=font, fill=255)
+        draw.text((x, top + 25), '  | X % 7d | '%(dct[10]), font=font, fill=255)
         
         # Display image.
         disp.image(image)
@@ -51,7 +66,7 @@ def standby():
     ### end on info
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top + 0), "%20s"%'Sensor Monitor v1.0', font=font, fill=255)
-    draw.text((x, top + 16), "     IP: " + IP, font=font, fill=255)
+    draw.text((x, top + 16), "  IP: %s  "%IP, font=font, fill=255)
     draw.text((x, top + 25), "   -- standing by --   " , font=font, fill=255)
     disp.image(image)
     disp.show()
@@ -60,7 +75,7 @@ def standby():
 def shutdown():
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top + 0), "%20s"%'Sensor Monitor v1.0', font=font, fill=255)
-    draw.text((x, top + 16), "     IP: " + IP, font=font, fill=255)
+    draw.text((x, top + 16),  "  IP: %s  "%IP, font=font, fill=255)
     draw.text((x, top + 25), "    -- shut down --    ", font=font, fill=255)
     disp.image(image)
     disp.show()  
