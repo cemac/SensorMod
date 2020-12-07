497c23cdcd893787cc588369c207686d33c20c8c
Daniel Ellis <daniel.ellis.research@gmail.com>
2020-12-07 22:44:36 +0000

    testsubmod3

--- oled/__init__.py
+++ oled/__init__.py
@@ -35,9 +35,9 @@ def updatedata(date, dct):
 
         draw.text((x, top + 0), "%20s"%date, font=font, fill=255)
         
-        draw.text((x, top + 8), 'PM1 % 7s| TEMP % 4dC'%(dct[4],dct[7]), font=font, fill=255)
-        draw.text((x, top + 16),'PM2.5 % 5s| RH % 6d'%(dct[5],dct[8]), font=font, fill=255)
-        draw.text((x, top + 25), 'PM10 % 6s| X % 7d'%(dct[6],dct[10]), font=font, fill=255)
+        draw.text((x, top + 8), 'PM1   % .2e| TEMP % 4dC'%(dct[4],dct[7]), font=font, fill=255)
+        draw.text((x, top + 16),'PM2.5 %.2e| RH % 6d'%(dct[5],dct[8]), font=font, fill=255)
+        draw.text((x, top + 25), 'PM10  % .2e| X % 7d'%(dct[6],dct[10]), font=font, fill=255)
         
         # Display image.
         disp.image(image)
@@ -52,7 +52,7 @@ def standby():
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top + 0), "%20s"%'Sensor Monitor v1.0', font=font, fill=255)
     draw.text((x, top + 16), "     IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 25), " -- standing by --  " + IP, font=font, fill=255)
+    draw.text((x, top + 25), "   -- standing by --   " , font=font, fill=255)
     disp.image(image)
     disp.show()
     
