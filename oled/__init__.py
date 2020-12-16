60b0ba0cadbfa107863e3cbf925ccfa3aecceaab
Dan Ellis <d.ellis@leeds.ac.uk>
2020-12-16 21:12:56 +0000

    Update __init__.py

--- oled/__init__.py
+++ oled/__init__.py
@@ -8,7 +8,7 @@ os.system('dtoverlay i2c-gpio bus=2 i2c_gpio_sda=22 i2c_gpio_scl=23')
 i2c = I2C(2) # use software bus i2c-2
 
 ### setup ###
-if '3c' not in os.popen('i2detect -y 2').read()
+if '3c' not in os.popen('i2detect -y 2').read():
         raise ImportError
 
 
