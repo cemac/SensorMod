8b7468d2c6783fe54fd34ee2244ae73815dc594f
Chris Symonds <c.c.symonds@leeds.ac.uk>
2021-02-17 12:33:41 +0000

    Connectivity fixes for sensor and upload code for serverpi to use module repo - Separated out connectivity tests so that time and git updates arte dependent on internet connection, and data upload attempts are dependent on serverpi connection. Added serverpi upload code so serverpi can use sensormod repo when main program updated

--- upload/__init__.py
+++ upload/__init__.py
@@ -11,7 +11,7 @@ def online():
 
     cmd = '''
     PINGS=2
-    TESTIP=10.3.141.1
+    TESTIP=8.8.8.8
     if ( ping -c $PINGS $TESTIP > /dev/null ) then
         echo "1"
     else
@@ -20,6 +20,19 @@ def online():
     '''
 
     return int(os.popen(cmd).read())
+
+def connected():
+    cmd = '''
+    PINGS=2
+    TESTIP=10.3.141.1
+    if ( ping -c $PINGS $TESTIP > /dev/null ) then
+        echo "1"
+    else
+        echo "0"
+    fi
+    '''
+    return int(os.popen(cmd).read())
+
 
 
 def readpassphrase(__RDIR__):
@@ -206,3 +219,117 @@ def sync(SERIAL,conn):
                 os.remove(source)
 
     return success
+
+def stage(SERIAL,__RDIR__):
+
+    import sqlite3
+    from datetime import datetime, date
+
+    DATE = date.today().strftime("%d%m%Y")
+    TIME = datetime.utcnow().strftime("%H%M%S")
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+
+    hostname = os.popen('hostname').read().strip()
+
+    newfilename = 'server_'+hostname+'_'+timestamp+'.db'
+    newfiledir = '/home/serverpi/datastaging'
+
+    file_name = os.path.join(__RDIR__,'server.db')
+
+    if not os.path.exists(newfiledir):
+        os.makedirs(newfiledir)
+
+    newfile = os.path.join(newfiledir,newfilename)
+
+    if os.path.exists(newfile):
+        os.remove(newfile)
+
+    try:
+        conn_dest = sqlite3.connect(newfile)
+    except:
+        print("Unable to create new file")
+        return False
+
+    try:
+        buildtables(conn_dest)
+    except:
+        print("Unable to write to new db")
+        conn_dest.close()
+        return False
+
+    cursor_dst = conn_dest.cursor()
+
+    cursor_dst.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    table_list=[]
+    for table_item in cursor_dst.fetchall():
+        table_list.append(table_item[0])
+
+    cmd = "attach ? as toMerge"
+    cursor_dst.execute(cmd, (os.path.join(__RDIR__,'server.db'), ))
+
+    for table_name in table_list:
+
+        try:
+            cmd = "INSERT INTO {0} SELECT * FROM toMerge.{0};".format(table_name)
+            cursor_dst.execute(cmd)
+            conn_dest.commit()
+            success = True
+        except sqlite3.OperationalError:
+            print("ERROR!: Merge Failed for " + table_name)
+            success = False
+        finally:
+            if table_name == table_list[-1]:
+                cmd = "detach toMerge"
+                cursor_dst.execute(cmd, ())
+
+    if not success:
+        conn_dest.close()
+        return success
+
+    else:
+        data = [(SERIAL,TIME,DATE,)]
+
+        try:
+            conn_dest.executemany("INSERT INTO PUSH (SERIAL,TIME,DATE) VALUES(?, ?, ?);", data )
+
+            conn_dest.commit()
+
+        except:
+            print ("Error! Could not write staging data to PUSH table at {}".format(timestamp))
+            success = False
+
+        conn_dest.close()
+
+    return success
+
+def upload():
+
+    from .sqlMerge import sqlMerge
+    from glob import glob
+
+    merge=sqlMerge()
+
+    dataloc = '/home/serverpi/datastaging'
+
+    sensorfiles = glob(os.path.join(dataloc,'sensor*.db'))
+
+    serverfiles = glob(os.path.join(dataloc,'server*.db'))
+
+    if len (serverfiles) > 1:
+        for file in serverfiles[1:]:
+            sensorfiles.append(file)
+    elif len (serverfiles) < 1:
+        print ("Could not find server.db file for merge")
+        return False
+
+    print (serverfiles[0])
+    print (sensorfiles)
+
+    #Merge the various DB and upload to AWS
+    success = merge.mergelist(serverfiles[0], sensorfiles)
+
+    if success:
+        return True
+    else:
+        return False
